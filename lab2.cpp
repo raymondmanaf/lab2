d8a763defe8c1809511d04278526d62d77a9a0dc
raymondmanaf <91324374+raymondmanaf@users.noreply.github.com>
2021-10-08 02:06:09 -0400

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,36 @@
+//============================================================================
+// Name        : lab2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+float MyNums[5]={2.1, 4.5, 25.2, -30, 94};
+float MyMax, MyMin;
+MyMax = MyNum[0];
+MyMin = MyNum[0];
+for (int i=0; i<5; i++)
+{
+	if (MyNum[i] > MyMax)
+		MyMax = MyNum[i];
+	if (MyNum[i] < MyMin)
+		MyMin = MyNum[i];
+}
+
+char UserInput;
+cout << "Please type a number 0 or 9"
+
+cin >> UserInput;
+
+switch(UserInput)
+{
+	case '0': "Min value"
+	case '9': "Max value"
+	default: "Please input a number 0 or 9"
+}
+
+
